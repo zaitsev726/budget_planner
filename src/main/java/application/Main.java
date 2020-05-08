fbd1290a300310002d5c09721d9edc8193ec80eb
sweetlover47 <pandaforever47@gmail.com>
2020-05-08 16:20:26 +0700

    merged

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,6 +2,6 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        new MainFrame(new Controller(new BudgetPlannerService(new InMemoryRepository())));
+        //new MainFrame(new Controller(new BudgetPlannerService(new InMemoryRepository())));
     }
 }
