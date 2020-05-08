f5ba016666f0d577e551487ea9fe1bf30b42bf58
sweetlover47 <pandaforever47@gmail.com>
2020-05-08 15:00:32 +0700

    made structure of frame

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -4,16 +4,23 @@ import controller.Controller;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class MainFrame extends JFrame {
     private JPanel totalPanel;
     private JPanel categoryListPanel;
     private JPanel turnoverListPanel;
+    private JLabel label1;
+    private JPanel pieChartPanel;
+    private JButton button1;
+    private JButton button2;
     private PieChart pieChart1;
     public static final int SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
     public static final int SCREEN_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+    private Controller controller;
 
-    public MainFrame(Controller controller) {
+    public MainFrame(Controller c) {
+        controller = c;
         setTitle("Delete statement");
         setLocation(
                 (SCREEN_WIDTH - totalPanel.getPreferredSize().width) / 2,
@@ -26,7 +33,18 @@ public class MainFrame extends JFrame {
     }
 
     private void createUIComponents() {
-        // TODO: place custom component creation code here
-        pieChart1 = new PieChart(30);
+        int r = 255, g = 0, b = 0;
+        ArrayList colorList = new ArrayList<>();
+        ArrayList values = new ArrayList<>();
+        new Color(255, 0, 0);
+        for (int i = 0; i < 10; ++i) {
+            values.add(10.0);
+            colorList.add(new Color(r, g, b));
+
+
+        }
+        pieChart1 = new PieChart(values, colorList);
+        pieChart1.setPreferredSize(new Dimension(150, 150));
+
     }
 }
