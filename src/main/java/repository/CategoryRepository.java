b3de604dd92df92f75c6f2de9e6b20c758f830ed
Aleksandr <zaitsev726@gmail.com>
2020-05-17 20:00:22 +0700

    adding category test, removing old classes

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,30 +1,5 @@
 package repository;
 
-import categories.Category;
-import repository.CategoryRepository;
-
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 public class CategoryRepository{
-    EntityManagerFactory emf;
-    public CategoryRepository(){
-        emf = Persistence.createEntityManagerFactory("model");
-    }
 
-    public void deleteCategory(Category category) {
-        EntityManager manager = emf.createEntityManager();
-        manager.getTransaction().begin();
-
-    }
-
-    public void saveCategory(Category category) {
-
-    }
-
-    public void editCategory(Category category) {
-
-    }
 }
--- src/test/java/entityTest/CategoryTest.java
+++ src/test/java/entityTest/CategoryTest.java
@@ -1,52 +1,26 @@
 package entityTest;
 
-import categories.Category;
+import entities.Category;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Date;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class CategoryTest {
-    public Category category;
+    private Category category;
 
     @Before
     public void setCategory(){
-        category = new Category("RandomName");
+        category = new Category();
+        category.setCategoryName("RandomName");
+        category.setIdCategory((long) 1);
+        category.setCurrentSum(100);
     }
 
-
     @Test
-    public void testDefaultValues(){
+    public void testParametrs(){
         assertEquals(category.getCategoryName(), "RandomName");
-        assertEquals(category.getCurrentSum(), 0);
-        assertEquals(category.getExpenseHistory().size(), 0);
-        assertEquals(category.getIncomeHistory().size(), 0);
-    }
-
-    @Test
-    public void testExpense(){
-        category.addNewExpense(100);
-        assertEquals(category.getExpenseHistory().size(), 1);
-        Date date = new Date();
-        category.addNewExpense(500, date);
-        assertEquals(category.getExpenseHistory().size(), 2);
-        assertEquals(category.getCurrentSum(), -600);
-        assertTrue(category.getExpenseHistory().containsKey("Расход: -500 " + date));
-        assertEquals(category.getExpenseHistory().get("Расход: -500 " + date),date);
-    }
-
-    @Test
-    public void testIncome(){
-        category.addNewIncome(200);
-        assertEquals(category.getCurrentSum(), 200);
-        Date date = new Date();
-        category.addNewIncome(700, date);
-        assertEquals(category.getIncomeHistory().size(), 2);
-        assertEquals(category.getCurrentSum(), 900);
-        assertTrue(category.getIncomeHistory().containsKey(("Доход: +700 " + date)));
-        assertEquals(category.getIncomeHistory().get("Доход: +7 00 " + date),date);
+        assertEquals((long) category.getIdCategory(), (long) 1);
+        assertEquals(category.getCurrentSum(), 100);
     }
 }
